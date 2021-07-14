598beb29e1605f49c53a67f12d17cb40ba0a2126
David <davidsantander9@gmail.com>
2021-07-13 20:09:02 -0500

    Add Dashboard routes and data

--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const MarvelScreen = () => {
     return (
         <div>
-            
+            <h1>Marvel Screen</h1>
         </div>
     )
 }
---
+++ src/routers/DashboardRoutes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { DcScreen } from '../components/dc/DcScreen';
+import { HeroScreen } from '../components/heroes/HeroScreen';
+import { MarvelScreen } from '../components/marvel/MarvelScreen';
+import { Navbar } from '../components/ui/NavBar';
+
+export const DashboardRoutes = () => {
+    return (
+        <>
+            <Navbar/>
+            <div>
+                <Switch>
+                    <Route exact path="/marvel" component={ MarvelScreen }/>
+                    <Route exact path="/marvel:heroId" component={ HeroScreen }/>
+                    <Route exact path="/dc" component={ DcScreen }/>
+                    <Route exact path="/dc:heroId" component={ HeroScreen }/>
+                    <Redirect to="/marvel"/>
+                </Switch>
+            </div>
+        </>
+    )
+}
